d502a3e44b306d11784952b6ce0d9038c396080d
kutay000 <kutay_byb70@outlook.com>
2026-01-06 15:24:07 +0300

    Display task titles and completion status in UI

--- app/src/main/java/com/yeditepe/finalexam/ui/TaskListScreen.kt
+++ app/src/main/java/com/yeditepe/finalexam/ui/TaskListScreen.kt
@@ -14,10 +14,12 @@ import com.yeditepe.finalexam.viewmodel.TaskViewModel
 fun TaskListScreen(viewModel: TaskViewModel = viewModel()) {
 
     // TODO 3: Read task list from ViewModel
-
+    val tasks = viewModel.taskList
     Column {
         // TODO 4: Display task titles and completion status
         // Use a simple Column or LazyColumn
+        tasks.forEach { task -> Text(text = "${task.title} - ${task.isCompleted}")
+        }
     }
 }
 
@@ -28,6 +30,7 @@ fun TaskRow(task: Task, navController: NavController) {
         text = task.title,
         modifier = Modifier.clickable {
             // TODO 3: Navigate to detail screen with task title
+            navController.navigate("taskDetail/${task.title}")
         }
     )
 }
